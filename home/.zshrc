c3f60b927d3bfaf5a3ce04d702ba33444712f709
András Németh <andrasnemeth@users.noreply.github.com>
2015-08-14 22:20:22 +0200

    Add utility scripts for wireless configuration

--- home/.zshrc
+++ home/.zshrc
@@ -1,1 +1,3 @@
 source ~/.zprezto/runcoms/zshrc
+
+alias wpa="sudo wpa_supplicant -B -i enp0s25 -c /etc/wpa_supplicant/wpa_supplicant.conf"
--- home/systemd/install.sh
+++ home/systemd/install.sh
@@ -1,3 +1,6 @@
 #!/usr/bun/env bash
 sudo cp ~/systemd/pacman-synchronize.* /etc/systemd/system/
 sudo systemctl enable pacman-synchronize.timer
+
+sudo cp ~/systemd/00-wireless-dhcp.conf /etc/systemd/network/
+sudo systemctl enable wpa_supplicant@wlo1
